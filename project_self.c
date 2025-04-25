4ed0f57a63bde7fbccf23ce78ccd9e78b386073b
amirho3einmov <amirmov85@gmail.com>
2025-04-25 21:54:18 +0330

    new

--- project_self.c
+++ project_self.c
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <windows.h>
+#define CL_RED "\033[1;31m"
+#define CL_BLUE "\033[1;34m"
+#define CL_GREEN "\033[1;32m"
+#define CL_MAGENTA "\033[1;35m"
+#define cl_rang "\033[1;40m"
+#define CL_DEFAULT "\033[0m"
+
 using namespace std;
- int main(){
-    cout << "hello world";
- }
